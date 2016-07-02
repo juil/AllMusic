8166f125383c154bf9453c5fca338ebc1cfd5029
Juil Yoon <jiy.juil@gmail.com>
2016-07-02 19:57:07 -0400

    Add onClickListener to song

--- app/src/main/java/com/juilyoon/allmusic/PlaylistActivity.java
+++ app/src/main/java/com/juilyoon/allmusic/PlaylistActivity.java
@@ -1,7 +1,10 @@
 package com.juilyoon.allmusic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class PlaylistActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class PlaylistActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playlist);
+
+        LinearLayout song1 = (LinearLayout) findViewById(R.id.song_01);
+        song1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nowPlaying = new Intent(PlaylistActivity.this, NowPlayingActivity.class);
+                startActivity(nowPlaying);
+            }
+        });
     }
 }
