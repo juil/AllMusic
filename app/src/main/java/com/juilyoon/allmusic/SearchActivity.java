24865171006dca4172808ec96048f94451887794
Juil Yoon <jiy.juil@gmail.com>
2016-07-02 22:27:42 -0400

    Add song to search activity

--- app/src/main/java/com/juilyoon/allmusic/SearchActivity.java
+++ app/src/main/java/com/juilyoon/allmusic/SearchActivity.java
@@ -1,7 +1,10 @@
 package com.juilyoon.allmusic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        LinearLayout song = (LinearLayout) findViewById(R.id.song_01);
+        song.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nowPlaying = new Intent(SearchActivity.this, NowPlayingActivity.class);
+                startActivity(nowPlaying);
+            }
+        });
     }
 }
